f1ce62d47d139cd63ee6f851adff8bbf7f307bcb
petit-fourmis <159149666+petit-fourmis@users.noreply.github.com>
2024-07-02 13:59:01 +0000

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -15,6 +15,8 @@ import environ
 import dj_database_url
 
 
+env = environ.Env()
+environ.Env.read()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
