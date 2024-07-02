633df6c7ad0f9098f682c0b4da395213f8df8bae
petit-fourmis <159149666+petit-fourmis@users.noreply.github.com>
2024-07-02 14:00:58 +0000

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-76e@fwmog-*9(@uzs7%6d4w@z@z$+3&e272)1*^x-#*u%fn=d1"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
